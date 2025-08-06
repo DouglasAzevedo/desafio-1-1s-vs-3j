ecd1940ca75fdae5d5dfc7b507afeb9f9573d184
Douglas <douglaaa.s@hotmail.com>
2025-08-06 20:30:32 -0300

    Desafio codecon análise API

---
+++ src/test/java/br/com/desafio/codecon/CodeconApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafio.codecon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeconApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
